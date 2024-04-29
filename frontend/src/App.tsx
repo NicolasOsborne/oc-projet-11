cff1d020323ab7d4792146773bc9f7c23c925c93
Nicolas Osborne <nho.osborne@gmail.com>
2024-04-29 17:58:59 +0200

    Mise en place du Router et des différentes pages (Home, SignIn, User)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import User from './pages/User'
+
 function App() {
   return (
-    <>
-      <h1>Argent Bank</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/user' element={<User />} />
+      </Routes>
+    </Router>
   )
 }
 
